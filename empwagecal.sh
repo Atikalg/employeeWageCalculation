2bf2b130a17feebd7d22d38f114926b6f94b9853
atikalg <who.atik@gmail.com>
2022-03-22 16:56:37 +0530

    using case to check employee attendance

--- empwagecal.sh
+++ empwagecal.sh
@@ -19,3 +19,32 @@ workingparttime ()
 	hoursperday=$((totalworkhours/(totaldays/2)))
 	echo "Toral work hours per day : $hoursperday"
 }
+
+case $empwAttendance in
+
+	0)
+                echo "Employee was absent"
+                echo "The employee salary = 0"
+					;;
+	1)
+		echo "Employee is present "
+		if(($totalworkhours>=100 || $totaldays==20))
+		then
+			echo "The employees salary = Rs.$dailywage"
+			workingHoursFulltime
+		else
+			echo "Working are not fully completed"
+		fi
+					;;
+	2)
+		echo "Employee was part time duty "
+		if(($totalworkhours>=100 || $totaldays==20))
+		then
+			echo "The employee salary = Rs.$parttimewage"
+			workingHoursPartTimeHour
+		else
+			echo "Working are not fully completed"
+		fi
+   					;;
+
+esac
