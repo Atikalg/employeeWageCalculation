24924c288db0af6d97cdfb1d63d0ff86ab53e3d8
atikalg <who.atik@gmail.com>
2022-03-22 16:43:52 +0530

    Part time wage calculation

--- empwagecal.sh
+++ empwagecal.sh
@@ -4,4 +4,18 @@ empAttendence=$((RANDOM%2+1))
 wageperhour=20
 fulldayhours=8
 parttimehour=4
+hoursperday=0
 dailywage=$(($wageperhour*100*20))
+parttimewage=$(($wageperhour*100*10))
+
+workinghoursfulltime ()
+{
+	hoursperday=$((totalworkhours/totaldays))
+	echo "Totak work hours per day : $hoursperday"
+}
+
+workingparttime ()
+{
+	hoursperday=$((totalworkhours/(totaldays/2)))
+	echo "Toral work hours per day : $hoursperday"
+}
